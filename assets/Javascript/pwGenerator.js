fe95832cda00c4c1b08d4b61d45de0b8aa3bf067
Cake <mcgrail.shauna@gmail.com>
2019-12-02 13:19:15 -0700

    updated javascript code to use for loop instead of individual Math.random statements. Added HTML and CSS and tested copy function.

--- assets/Javascript/pwGenerator.js
+++ assets/Javascript/pwGenerator.js
@@ -1,17 +1,32 @@
 //Define variables here:
-//let characters = [];
-//let choices = [];
-let passwordLength;
+
+
+let charSelect = [];
+let passwordLength = 0;
+let randomChar = 0;
+let lcConfirm;
+let ucConfirm;
+let symbConfirm;
+let numConfirm;
+let finalPass = "";
+
 
 //Arrays of possible characters 
 const ucLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lcLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const symb = ["!", "@", "#", "$", "%", "&"];
 
-//Declare function to start script
-function andStart() {
 
+//Declare function to start script when Generate Button is clicked
+function andStart() {
+    howMany();
+    askChar();
+ 
+
+};
+
+function howMany() {
     //Ask user to select number of characters desired in password    
     let passwordLength = prompt('How many characters would you like your password to be?');
 
@@ -19,12 +34,13 @@ function andStart() {
     while (passwordLength <= 7 || passwordLength >= 129) {
         alert("Password must be between 8 and 128 characters.");
         passwordLength = prompt('How many characters would you like your password to be?');
-    }
+    };
 
     console.log(passwordLength);
+};
 
+function askChar() {
     //ask user additional parameters for password
-
     const ucConfirm = confirm('Would you like uppercase letters?');
     console.log(ucConfirm);
     const lcConfirm = confirm('Would you like lowercase letters?');
@@ -34,49 +50,57 @@ function andStart() {
     const numConfirm = confirm('Would you like numbers?');
     console.log(numConfirm);
 
+    //generate array of selected characters
+    if (lcConfirm) {
+        lcArr = charSelect.concat(lcLetters);
+        charSelect = lcArr;
+        console.log(charSelect);
+    };
 
-    //function to generate random lowercase characters
-    function getRandomLc() {
-        if (lcConfirm) {
-            return lcLetters[Math.floor(Math.random() * lcLetters.length)];
-        }
 
-    }
+    if (ucConfirm) {
+        ucArr = charSelect.concat(ucLetters);
+        charSelect = ucArr;
+        console.log(charSelect);
+    };
 
-    console.log(getRandomLc());
 
-    //function to generate random uppercase letters
-    function getRandomUc() {
-        if (ucConfirm) {
-            return ucLetters[Math.floor(Math.random() * ucLetters.length)];
-        }
-    }
+    if (numConfirm) {
+        numArr = charSelect.concat(num);
+        charSelect = numArr;
+        console.log(charSelect);
+    };
 
-    console.log(getRandomUc());
 
-    //function to generate random numbers
-    function getRandomNum() {
-        if (numConfirm) {
-            return num[Math.floor(Math.random() * num.length)];
-        }
-    }
+    if (symbConfirm) {
+        symbArr = charSelect.concat(symb);
+        charSelect = symbArr;
+        console.log(charSelect);
+    };
 
-    console.log(getRandomNum());
-
-    //function to generate random symbols
-    function getRandomSymb() {
-        if (symbConfirm) {
-            return symb[Math.floor(Math.random() * symb.length)];
-        }
-    }
-    console.log(getRandomSymb());
 
     //statement in case all of the above choices are false
     if ((symbConfirm === false) && (numConfirm === false) && (lcConfirm === false) && (ucConfirm === false)) {
         alert("You need to select at least one character type to continue.");
         andStart();
-    }
+    };
 
-}
+    //code that will create the fnal password
+    for (let i = 0; i < passwordLength; i++) {
+        randomChar = Math.floor(Math.random() * charSelect.length);
+        finalPass += charSelect[randomChar];
+    };
 
-andStart();
+    console.log(finalPass);
+    document.getElementById("password").value = finalPass;
+
+};
+
+//function to copy password to clipboard
+function copyPassword() {
+    var copyText = document.getElementById("password");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Passowrd Copied!")
+};
